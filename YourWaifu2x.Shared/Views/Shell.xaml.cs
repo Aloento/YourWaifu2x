840df074c207853605080a106920c2aedc1d3509
Aloento <11802769+soarteam@users.noreply.github.com>
2021-05-19 20:36:05 +0200

    Uno.Gallery

--- YourWaifu2x.Shared/Views/Shell.xaml.cs
+++ YourWaifu2x.Shared/Views/Shell.xaml.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -13,16 +13,16 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
 
-namespace YourWaifu2x
+namespace YourWaifu2x.Views
 {
     /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// 可用于自身或导航至 Frame 内部的空白页。
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Shell : Page
     {
-        public MainPage()
+        public Shell()
         {
             this.InitializeComponent();
         }
