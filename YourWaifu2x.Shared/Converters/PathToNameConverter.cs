8a0906fcc0a8dae9a09c0569f25a65e95590f62b
Aloento <11802769+soarteam@users.noreply.github.com>
2021-05-23 15:39:13 +0200

    VS的Bug多

--- YourWaifu2x.Shared/Converters/PathToNameConverter.cs
+++ YourWaifu2x.Shared/Converters/PathToNameConverter.cs
@@ -1,17 +1,17 @@
 namespace YourWaifu2x.Converters {
     using System;
+    using System.IO;
     using Windows.UI.Xaml.Data;
 
     /// <summary>
     /// Use this converter to debug data bindings in your xaml.
     /// </summary>
-    public class DebugConverter : IValueConverter {
+    public class PathToNameConverter : IValueConverter {
         public object Convert(object value, Type targetType, object parameter, string language) =>
-            // Put a breakpoint here to inspect values from the ViewModel to the View.
-            value;
+            Path.GetFileName(value as string);
 
         public object ConvertBack(object value, Type targetType, object parameter, string language) =>
-            // Put a breakpoint here to inspect values from the View to the ViewModel.
-            value;
+        // Put a breakpoint here to inspect values from the View to the ViewModel.
+        value;
     }
 }
--- YourWaifu2x.UWP/Views/GeneralPages/SelectingImages.xaml.cs
+++ YourWaifu2x.UWP/Views/GeneralPages/SelectingImages.xaml.cs
@@ -2,32 +2,26 @@ namespace YourWaifu2x.Views.GeneralPages {
     using System.Collections.ObjectModel;
     using System.IO;
     using Windows.UI.Xaml;
-    using Windows.UI.Xaml.Controls;
     using Windows.UI.Xaml.Input;
 
-    public class Images {
-        public FileStream FileStream { get; set; }
-        public string FileName => Path.GetFileName(FileStream.Name);
-    }
-
     [Page(PageCategory.None, "Selecting Images")]
-    public sealed partial class SelectingImages : Page {
+    public sealed partial class SelectingImages {
         public SelectingImages() => InitializeComponent();
-        public ObservableCollection<Images> ImageListData = new ObservableCollection<Images>();
+        public readonly ObservableCollection<FileStream> ImageListData = new ObservableCollection<FileStream>();
 
         private void Selecting_OnLoaded(object sender, RoutedEventArgs e) {
             ImagesList.ItemsSource = ImageListData;
+            ImageListData.Add(File.OpenRead("YourWaifu2x.exe"));
 
-            ImageListData.Add(new Images() {
-                FileStream = File.OpenRead("YourWaifu2x.exe")
-            });
+            ImagesList.SelectionChanged += (o, args) =>
+                AllCountLabel.Label = ImagesList.SelectedItems.Count + "/" + ImageListData.Count;
 
             var deleteCommand = new StandardUICommand(StandardUICommandKind.Delete);
             deleteCommand.ExecuteRequested += (command, args) => {
                 if (ImagesList.SelectedItems.Count == 0)
                     return;
                 foreach (var i in ImagesList.SelectedItems) {
-                    _ = ImageListData.Remove((Images)i);
+                    _ = ImageListData.Remove((FileStream)i);
                 }
             };
             DeleteButton.Command = deleteCommand;
