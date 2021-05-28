b2fc68b3fabae486ebca0cdb335c9cce94191eb6
Aloento <11802769+soarteam@users.noreply.github.com>
2021-05-28 14:04:50 +0200

    好耶

--- YourWaifu2x.UWP/Views/GeneralPages/SettingWaifu2x.xaml.cs
+++ YourWaifu2x.UWP/Views/GeneralPages/SettingWaifu2x.xaml.cs
@@ -1,5 +1,6 @@
 namespace YourWaifu2x.Views.GeneralPages {
     using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Input;
     using Entities.Data;
 
     [Page(PageCategory.None, "Setting Waifu2x")]
@@ -9,6 +10,11 @@ namespace YourWaifu2x.Views.GeneralPages {
         public SettingWaifu2x() => InitializeComponent();
 
         private void Setting_OnLoaded(object sender, RoutedEventArgs e) {
+
+            var nextCommand = new StandardUICommand(StandardUICommandKind.Forward);
+            nextCommand.ExecuteRequested += (command, args) =>
+                (Application.Current as App)?.ShellNavigateTo(App.FindMyPage<Exporting>());
+            NextButton.Command = nextCommand;
 
         }
     }
