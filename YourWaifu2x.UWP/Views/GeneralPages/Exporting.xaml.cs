a263a6177c965aea09b8bb4f730c008b045a65d2
Aloento <11802769+soarteam@users.noreply.github.com>
2021-05-29 11:41:33 +0200

    好耶

--- YourWaifu2x.UWP/Views/GeneralPages/Exporting.xaml.cs
+++ YourWaifu2x.UWP/Views/GeneralPages/Exporting.xaml.cs
@@ -1,6 +1,7 @@
 namespace YourWaifu2x.Views.GeneralPages {
     using System;
     using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
     using Entities.Data;
     using Extensions;
 
@@ -16,6 +17,18 @@ namespace YourWaifu2x.Views.GeneralPages {
 
         private void Exporting_OnLoaded(object sender, RoutedEventArgs e) {
 
+        }
+
+        private void WaitingList_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void FinishedList_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            throw new NotImplementedException();
+        }
+
+        private void ErrorList_OnSelectionChanged(object sender, SelectionChangedEventArgs e) {
+            throw new NotImplementedException();
         }
     }
 }
