219e85f9de7343509c4a88f75c4c3134ac240dc3
Aloento <11802769+soarteam@users.noreply.github.com>
2021-06-03 10:19:09 +0200

    清理

--- YourWaifu2x.UWP/App.xaml.cs
+++ YourWaifu2x.UWP/App.xaml.cs
@@ -3,9 +3,7 @@ namespace YourWaifu2x {
     using System.Collections.Generic;
     using System.Linq;
     using System.Reflection;
-    using System.Threading.Tasks;
     using Microsoft.Extensions.Logging;
-    using ShowMeTheXAML;
     using Uno.Extensions;
     using Uno.Logging;
     using Windows.ApplicationModel.Activation;
@@ -20,26 +18,16 @@ namespace YourWaifu2x {
     using MUXC = Microsoft.UI.Xaml.Controls;
     using MUXCP = Microsoft.UI.Xaml.Controls.Primitives;
 
-    /// <summary>
-    /// Provides application-specific behavior to supplement the default Application class.
-    /// </summary>
     public sealed partial class App {
         private static MyPage[] myPages;
         private Shell shell;
 
-        /// <summary>
-        /// Initializes the singleton application object.  This is the first line of authored code
-        /// executed, and as such is the logical equivalent of main() or WinMain().
-        /// </summary>
         public App() {
 #if !WINDOWS_UWP
             Uno.UI.FeatureConfiguration.ApiInformation.NotImplementedLogLevel = LogLevel.Debug; // Raise not implemented usages as Debug messages
 #endif
 
-            ConfigureFilters(LogExtensionPoint.AmbientLoggerFactory);
-            XamlDisplay.Init(GetType().Assembly);
             InitializeComponent();
-
             Suspending += (s, e) => e.SuspendingOperation.GetDeferral().Complete();
 
 #if __WASM__
@@ -47,11 +35,6 @@ namespace YourWaifu2x {
 #endif
         }
 
-        /// <summary>
-        /// Invoked when the application is launched normally by the end user.  Other entry points
-        /// will be used such as when the application is launched to open a specific file.
-        /// </summary>
-        /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e) {
             this.Log().Debug("Launched app.");
 
@@ -64,7 +47,6 @@ namespace YourWaifu2x {
                 Xamarin.Calabash.Start();
 #endif
 
-                //InitializeThemes();
                 Uno.Material.Resources.Init(this, colorPaletteOverride: new ResourceDictionary() { Source = new Uri("ms-appx:///Views/Colors.xaml") });
                 Uno.Cupertino.Resources.Init(this, null);
                 Resources.MergedDictionaries.Add(new ResourceDictionary() { Source = new Uri("ms-appx:///Views/Styles/TextBlock.xaml") });
@@ -79,13 +61,9 @@ namespace YourWaifu2x {
                 WaifuInstance.Init();
             }
 
-            // Ensure the current window is active
             window.Activate();
         }
 
-        /// <summary>
-        /// This method is used as the entry point when opening the app from an url.
-        /// </summary>
         protected override void OnActivated(IActivatedEventArgs args) {
             this.Log().Debug("Activated app.");
             base.OnActivated(args);
@@ -141,7 +119,6 @@ namespace YourWaifu2x {
             AddNavigationItems(nv);
 
             ShellNavigateTo(FindMyPage<OverviewPage>());
-            // navigation + setting handler
             nv.ItemInvoked += (s, e) => {
                 if (e.InvokedItemContainer.DataContext is MyPage sample)
                     ShellNavigateTo(sample, false);
@@ -183,72 +160,13 @@ namespace YourWaifu2x {
             }
 
             void NavViewItemVisualStateFix(MUXC.NavigationViewItem nvi) =>
-                // gallery#107: on uwp and uno, deselecting a NVI by selecting another NVI will leave the former in the "Selected" state
-                // to workaround this, we force reset the visual state when IsSelected becomes false
                 nvi.RegisterPropertyChangedCallback(MUXC.NavigationViewItemBase.IsSelectedProperty, (s, e) => {
                     if (!nvi.IsSelected) {
-                        // depending on the DisplayMode, a NVIP may or may not be used.
                         var nvip = VisualTreeHelperEx.GetFirstDescendant<MUXCP.NavigationViewItemPresenter>(nvi, x => x.Name == "NavigationViewItemPresenter");
                         _ = VisualStateManager.GoToState((Control)nvip ?? nvi, "Normal", true);
                     }
                 });
         }
-
-        /// <summary>
-        /// Configures global logging
-        /// </summary>
-        /// <param name="factory"></param>
-        private static void ConfigureFilters(ILoggerFactory factory) => factory
-                .WithFilter(new FilterLoggerSettings
-                    {
-                        { "Uno", LogLevel.Warning },
-                        { "Windows", LogLevel.Warning },
-                        { "YourWaifu2x", LogLevel.Debug },
-#if !DEBUG
-						{ "Windows.UI.Xaml", LogLevel.None },
-						{ "Windows.ApplicationModel.Core.CoreApplicationViewTitleBar", LogLevel.None },
-						{ "Uno.UI.DataBinding.BindingPropertyHelper", LogLevel.None },
-#endif
-
-						// Debug JS interop
-						// { "Uno.Foundation.WebAssemblyRuntime", LogLevel.Debug },
-
-						// Generic Xaml events
-						// { "Windows.UI.Xaml", LogLevel.Debug },
-						// { "Windows.UI.Xaml.VisualStateGroup", LogLevel.Debug },
-						// { "Windows.UI.Xaml.StateTriggerBase", LogLevel.Debug },
-						// { "Windows.UI.Xaml.UIElement", LogLevel.Debug },
-
-						// Layouter specific messages
-						// { "Windows.UI.Xaml.Controls", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.Layouter", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.Panel", LogLevel.Debug },
-						// { "Windows.Storage", LogLevel.Debug },
-
-						// Binding related messages
-						// { "Windows.UI.Xaml.Data", LogLevel.Debug },
-
-						// DependencyObject memory references tracking
-						// { "ReferenceHolder", LogLevel.Debug },
-
-						// ListView-related messages
-						// { "Windows.UI.Xaml.Controls.ListViewBase", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.ListView", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.GridView", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.VirtualizingPanelLayout", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.NativeListViewBase", LogLevel.Debug },
-						// { "Windows.UI.Xaml.Controls.ListViewBaseSource", LogLevel.Debug }, //iOS
-						// { "Windows.UI.Xaml.Controls.ListViewBaseInternalContainer", LogLevel.Debug }, //iOS
-						// { "Windows.UI.Xaml.Controls.NativeListViewBaseAdapter", LogLevel.Debug }, //Android
-						// { "Windows.UI.Xaml.Controls.BufferViewCache", LogLevel.Debug }, //Android
-						// { "Windows.UI.Xaml.Controls.VirtualizingPanelGenerator", LogLevel.Debug }, //WASM
-					}
-                )
-#if DEBUG
-                .AddConsole(LogLevel.Debug);
-#else
-				.AddConsole(LogLevel.Information);
-#endif
 
         public static IEnumerable<MyPage> GetPages() => myPages = myPages ?? Assembly.GetExecutingAssembly()
                            .DefinedTypes
