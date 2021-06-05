bddab0f39bbb15bfe5cd3af0651893865cef4340
Aloento <11802769+soarteam@users.noreply.github.com>
2021-06-05 04:52:31 +0200

    按钮

--- YourWaifu2x.Shared/Views/GeneralPages/OverviewPage.xaml.cs
+++ YourWaifu2x.Shared/Views/GeneralPages/OverviewPage.xaml.cs
@@ -1,11 +1,17 @@
 namespace YourWaifu2x.Views.GeneralPages {
-    using System;
-
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Input;
 
     [Page(PageCategory.None, "Overview")]
     public sealed partial class OverviewPage {
-        public OverviewPage() {
-            InitializeComponent();
+        public OverviewPage() => InitializeComponent();
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e) {
+            var nextCommand = new StandardUICommand(StandardUICommandKind.Forward);
+            nextCommand.ExecuteRequested += (command, args) =>
+                (Application.Current as App)?.ShellNavigateTo(App.FindMyPage<SelectingImages>());
+            ((Button)sender).Command = nextCommand;
         }
     }
 }
