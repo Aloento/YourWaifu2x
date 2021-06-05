62a2114f0618b90e56de55495b9ce0e9f65c6a7d
Aloento <11802769+soarteam@users.noreply.github.com>
2021-06-05 07:59:32 +0200

    删除C

--- YourWaifu2x.Shared/Views/SamplePages/AcrylicSamplePage.xaml.cs
+++
@@ -1,8 +0,0 @@
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
-
-namespace YourWaifu2x.Views.Samples {
-    [Page(PageCategory.Features, "Acrylic", Description = "AcrylicBrush is a translucent brush that can be used as background.", DocumentationLink = "https://docs.microsoft.com/en-us/windows/uwp/design/style/acrylic")]
-    public sealed partial class AcrylicSamplePage {
-        public AcrylicSamplePage() => InitializeComponent();
-    }
-}
